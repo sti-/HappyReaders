63097e6e26cac03ef078aeddc71e707eb05e18c0
sti- <cristi.naftanaila@gmail.com>
2014-11-16 03:47:35 +0200

    final update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,36 @@
-function showtestimonials() {
-    $.post("update", {randuri: "0", cate: "3"}, function (data) {
+function createTableRows(data) {
+    var rows = "";
+    for (var i = 0; i < data.length; i++) {
+        rows += '<tr><td>';
+        rows += data[i].companie;
+        rows += '</td><td>';
+        rows += data[i].user;
+        rows += '</td><td><div id="cristi" class="allmesaj">';
+        rows += data[i].mesaj;
+        rows += '</div></td><td>';
+        var date = new Date(data[i].datamesaj);
+        rows += date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+        rows += '</td><td>';
+        rows += data[i].trimis || "";
+        rows += '</td></tr>';
+    }
+    return rows;
+}
 
-        $("#raspuns").prepend(data);
-        $("table").mouseover( function (e) {
+function showtestimonials(cate) {
+    if (typeof(cate) == "undefined") { cate = 3; }
+    $.post("update", {randuri: "0", cate: cate}, function (data) {
+
+        $("#myTable").remove();
+
+        var tabel = '<table id="myTable">';
+        tabel += '<tr><th>Companie</th><th>User</th><th>Mesaj</th><th>Data</th><th></th></tr>';
+
+        tabel += createTableRows(data);
+
+        tabel += "</table>";
+        $("#raspuns").prepend(tabel);
+        $("table").mouseover(function(e) {
             var event = $(e.target);
             if (event.is("div")) {
                 event.addClass("showallmesaj");
@@ -15,25 +43,21 @@ function showsave() {
     var datatosave = $("form").serializeArray();
     $.post("savemessage", datatosave, function(data) {
         if (data=="yes") {
-            $("#myTable").remove();
-            $.post("update", {randuri: "0", cate: "3"}, function(data) {
-                $("#raspuns").prepend(data);
-                $("table").mouseover( function (e) {
-                    var event = $(e.target);
-                    if (event.is("div")) {
-                        event.addClass("showallmesaj");
-                    }
-                });
-            });
+            var cate = 3;
+            var tableElem = document.getElementById("myTable");
+            if (tableElem) {
+                cate = tableElem.rows.length - 1;
+            }
+            showtestimonials(cate);
          }
     })
 }
 
 function showmore() {
     var x = document.getElementById("myTable").rows.length;
-    $.post("updatemore", {randuri: x-1, cate: "3"}, function (data) {
-        $("#myTable").append(data);
-    })
+    $.post("update", {randuri: x - 1, cate: "3"}, function (data) {
+        $("#myTable").append(createTableRows(data));
+    });
     $("html, body").animate({ scrollTop: $(document).height() }, "slow");
 }
 
--- server.js
+++ server.js
@@ -31,72 +31,12 @@ app.post('/savemessage', function(req,res) {
 });
 
 app.post('/update', function(req, res) {
-    var tabel = '<table id="myTable">';
-    tabel += '<tr><td>Companie</td><td>User</td><td>Mesaj</td><td>Data</td><td></td></tr>';
-    var i = 0;
     Testimonial.find().sort({datamesaj: -1}).exec(function (err, testimoniale) {
         if (err) throw err;
         var start = Number(req.body.randuri);
         var end = start + Number(req.body.cate);
-        end = Math.min(end,testimoniale.length);
-        for (i=start; i<end; i++) {
-            tabel += '<tr><td>';
-            tabel += testimoniale[i].companie;
-            tabel += '</td><td>';
-            tabel += testimoniale[i].user;
-            tabel += '</td><td><div id="cristi" class="allmesaj">';
-            tabel += testimoniale[i].mesaj;
-            tabel += '</div></td><td>';
-            var date = new Date(testimoniale[i].datamesaj);
-            tabel += date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
-            tabel += '</td><td>';
-            var trimis = testimoniale[i].trimis;
-            if (trimis=="trimis") {
-            tabel += trimis;
-            }
-            tabel += '</td></tr>';
-        }
-        tabel += '</table>';
-        res.send(tabel);
-    })
-});
 
-app.post('/updatemore', function(req, res) {
-    var tabel = '';
-    tabel += '';
-    var i = 0;
-    var start = Number(req.body.randuri);
-    var end = start + Number(req.body.cate);
-    var catesunt = 0;
-    Testimonial.find(function (err, testimoniale) {
-        if (err) throw err;
-        testimoniale.forEach(function () {
-            catesunt++;
-        });
-        if (end > catesunt) {
-            end = catesunt;
-        }
-    })
-    Testimonial.find().sort({datamesaj: -1}).exec(function (err, testimoniale) {
-        if (err) throw err;
-        for (i=start; i<end; i++) {
-            tabel += '<tr><td>';
-            tabel += testimoniale[i].companie;
-            tabel += '</td><td>';
-            tabel += testimoniale[i].user;
-            tabel += '</td><td><div id="cristi" class="allmesaj">';
-            tabel += testimoniale[i].mesaj;
-            tabel += '</div></td><td>';
-            var date = new Date(testimoniale[i].datamesaj);
-            tabel += date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
-            tabel += '</td><td>';
-            var trimis = testimoniale[i].trimis;
-            if (trimis=="trimis") {
-                tabel += trimis;
-            }
-            tabel += '</td></tr>';
-        }
-        res.send(tabel);
+        res.send(testimoniale.slice(start, end));
     })
 });
 
